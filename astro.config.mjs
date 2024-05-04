e41298cff846bf395689d8273f859e122df3728d
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2024-05-04 19:53:20 +0100

    astrodb

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-import icon from "astro-icon"
+import icon from 'astro-icon'
+import db from '@astrojs/db'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+    db(),
+  ],
 })
